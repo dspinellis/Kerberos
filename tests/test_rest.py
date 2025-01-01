41e6b7d69c29bae609f4d30a2e7a8eb2b75ace67
Diomidis Spinellis <dds@aueb.gr>
2025-01-01 22:30:13 +0200

    Test multiple commands

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -2,6 +2,7 @@ from io import StringIO
 import pytest
 from unittest.mock import patch, call
 
+from alarmd import debug
 from alarmd.rest import app
 from alarmd.dsl import read_config
 from alarmd.state import event_processor
@@ -17,24 +18,41 @@ def client():
 
 def test_command_route(client):
     mock_file = StringIO(SETUP + """
+*:
+    CmdSecond > second
+    CmdOther > other
+    ;
+
 initial:
     | set_bit('Siren5', 1)
-    CmdSecond > second
+    quit > DONE
     ;
 
 second:
     | set_bit('Siren6', 0)
+    quit > DONE
+    ;
+
+other:
+    | set_bit('Siren6', 1)
     > DONE
+    ;
     """)
     with patch('RPi.GPIO.output') as mock_output, \
             patch('RPi.GPIO.setup') as mock_setup:
         initial_name = read_config(mock_file)
+
         response = client.get("/cmd/Second")
         assert response.status_code == 200
         assert response.json == {"CmdSecond": "OK"}
+
+        response = client.get("/cmd/Other")
+        assert response.status_code == 200
+        assert response.json == {"CmdOther": "OK"}
+
         event_processor(initial_name)
-        assert mock_output.call_count == 2
-        mock_output.assert_has_calls([call(5, 1), call(6, 0)])
+        assert mock_output.call_count == 3
+        mock_output.assert_has_calls([call(5, 1), call(6, 0), call(6, 1)])
 
 
 def test_404_route(client):
