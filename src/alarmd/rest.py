565903abb4f2d9dbe24b3873973c1f89769affb6
Diomidis Spinellis <dds@aueb.gr>
2025-01-03 13:42:01 +0200

    Restrict requests to localhost

--- src/alarmd/rest.py
+++ src/alarmd/rest.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, jsonify
+from flask import Flask, abort, jsonify, request
 
 from . import debug
 from .event_queue import event_queue
@@ -6,6 +6,12 @@ from .state import get_state, all_states
 
 # Flask setup
 app = Flask(__name__)
+
+def access_check():
+    """Only allow localhost requests."""
+    if request.remote_addr != '127.0.0.1':
+        abort(403) # Forbidden
+
 
 @app.route('/cmd/<name>', methods=['GET'])
 def rest_cmd(name):
@@ -19,16 +25,18 @@ def rest_cmd(name):
     Returns:
         str: eventname: "OK"
     """
+    access_check()
     event = f"Cmd{name}"
     debug.log(f"Queuing REST command event {event}")
     if not all_states.has_event_transition(event):
-        abort(404)
+        abort(404) # Not found
     event_queue.put(event)
     return jsonify({event: "OK"})
 
 
-@app.route('/state')
+@app.route('/state', methods=['GET'])
 def rest_status():
+    access_check()
     return jsonify({
         "state": get_state().get_name(),
     })
